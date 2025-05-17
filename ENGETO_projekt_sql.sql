99f3f84de4a704502851412c524327e24c0dbc59
ondra-pavlicek <ondra-pavlicek@seznam.cz>
2025-05-17 02:01:21 +0200

    Update ENGETO_projekt_sql.sql

--- ENGETO_projekt_sql.sql
+++ ENGETO_projekt_sql.sql
@@ -124,6 +124,7 @@ CREATE TABLE IF NOT EXISTS t_ondrej_pavlicek_project_sql_secondary_final AS
                    year,
                    gdp,
                    population,
+		   gini,	
                    gdp / population AS "gdp_per_cap"
    FROM   economies
    WHERE  ( country IN (SELECT DISTINCT country
