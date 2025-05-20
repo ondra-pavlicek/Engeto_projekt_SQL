7c9deb4ebd5307beea2023e697746c46b4999ef7
ondra-pavlicek <ondra-pavlicek@seznam.cz>
2025-05-20 23:10:04 +0200

    Update Q2_vyzkumny_doraz_2.sql
    nahrazeny staticke proměnné

--- Q2_vyzkumny_doraz_2.sql
+++ Q2_vyzkumny_doraz_2.sql
@@ -1,44 +1,52 @@
 /*
  * 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
  * */
---Dberaver idikuje chybu ve druhém poddotazu cte2 na řádcích odkazujících na hodnoty min_year, max_year indikuje ve slupci s čísly řádků s tím, že hodnoty slopce a tabulky nezná, možná mám víc štěstí než rozumu, ale AI mi říká, že TO mám správně. Třeba nefunguje správně nápověda v dbeaveru :-) Mám štěstí. Asi bych obešel pomocným dotazem tvořícím tabulky, proměnné nebo VIEW
-WITH year_range AS (
-    SELECT MIN(year) AS min_year, MAX(year) AS max_year
-    FROM t_ondrej_pavlicek_project_sql_primary_final
-    WHERE name IS NULL
-    UNION
-    SELECT MIN(year) AS min_year, MAX(year) AS max_year
-    FROM t_ondrej_pavlicek_project_sql_primary_final
-    WHERE name IS NOT NULL
-),
-cte2 AS (
-    SELECT price_value_avg, category_code, category_name, price_value, price_unit, year
-    FROM t_ondrej_pavlicek_project_sql_primary_final
-    WHERE year IN (
-        SELECT MAX(min_year) FROM year_range
-        UNION
-        SELECT MIN(max_year) FROM year_range
-    )
-    AND category_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
-)
-SELECT 
-    ROUND(t.income_value_avg / c.price_value_avg, 2) AS ratio,
-    CASE 
-        WHEN c.price_value_avg = 16.12 THEN 'kg chleba 2006'
-        WHEN c.price_value_avg = 24.24 THEN 'kg chleba 2018'
-        WHEN c.price_value_avg = 14.44 THEN 'l mléka 2006'
-        WHEN c.price_value_avg = 19.82 THEN 'l mléka 2018'
-        ELSE 'Neznámá hodnota'
-    END AS produkt_info,
-    COALESCE(t.industry_name, 'Neuvedeno') AS industry_name,
-    c.price_value_avg,
-    t.income_value_avg,
-    t.year
-FROM t_ondrej_pavlicek_project_sql_primary_final t
-JOIN cte2 c ON t.year = c.year
-WHERE t.year IN (
-    SELECT MAX(min_year) FROM year_range
-    UNION
-    SELECT MIN(max_year) FROM year_range
-)
-AND t.category_name IS NULL;
+--DOTAZ VYTVOŘÍ TABULKU, VYPOČÍTÁVAJÍCÍ POMĚR PRŮMĚRNÉ MZDY A PRŮMĚRNÉ JEDNOTKOVÉ CENY VYBRANÉHO PRODUKTU PRO KAŽDOU KATEGORII PRŮMYSLU V HORNÍM A DOLNÍ MEZI INTERVALU LET
+
+WITH year_range
+     AS (SELECT Min(year) 								AS min_year,
+                Max(year) 								AS max_year
+         FROM   t_ondrej_pavlicek_project_sql_primary_final
+         WHERE  NAME IS NULL
+         UNION
+         SELECT Min(year)								AS min_year,
+                Max(year) 								AS max_year
+         FROM   t_ondrej_pavlicek_project_sql_primary_final
+         WHERE  NAME IS NOT NULL),
+     food_price_value_avg_years
+     AS (SELECT price_value_avg,
+                category_code,
+                category_name,
+                price_value,
+                price_unit,
+                year
+         FROM   t_ondrej_pavlicek_project_sql_primary_final
+         WHERE  year IN (SELECT Max(min_year)
+                         FROM   year_range
+                         UNION
+                         SELECT Min(max_year)
+                         FROM   year_range)
+                AND category_name IN ( 'Chléb konzumní kmínový',
+                                       'Mléko polotučné pasterované' ))
+SELECT Round(t.income_value_avg / c.price_value_avg, 2) AS ratio,
+       c.price_unit
+       || ' '
+       || c.category_name
+       || ' '
+       || c.year                                        AS product_info,
+       COALESCE(t.industry_name, 'Neuvedeno')           AS industry_name,
+       c.price_value_avg,
+       t.income_value_avg,
+       t.year
+FROM   t_ondrej_pavlicek_project_sql_primary_final t
+       JOIN food_price_value_avg_years c
+         ON t.year = c.year
+WHERE  t.year IN (SELECT Max(min_year)
+                  FROM   year_range
+                  UNION
+                  SELECT Min(max_year)
+                  FROM   year_range)
+       AND t.category_name IS NULL
+ORDER  BY industry_name DESC,
+          product_info DESC,
+          year DESC; 
